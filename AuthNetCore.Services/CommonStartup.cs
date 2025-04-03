548bc17a81f4f85a80cb47cf201d8f37e0827151
Javier18121998 <javier.marin.garcia.linkedin01@hotmail.com>
2025-04-03 17:55:12 -0600

    Stablishing the rules of the architecture raw inner Behaviorals

--- AuthNetCore.Services/CommonStartup.cs
+++ AuthNetCore.Services/CommonStartup.cs
@@ -1,6 +1,12 @@
 ﻿using AuthNetCore.Data.Access;
 using AuthNetCore.Utilities.Globals;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AuthNetCore.Services
 {
@@ -31,7 +37,118 @@ namespace AuthNetCore.Services
                 );
             }
             else
-                logger.Error("Error, missing configuration 'MarketProductDB'");
+                logger.Error(Globals.LoggIssueConnection);
+        }
+
+        internal static void AddJwtAuthentication(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var jwtKey = configuration["Jwt:Key"];
+            if (string.IsNullOrEmpty(jwtKey))
+            {
+                throw new ArgumentNullException(nameof(jwtKey), Globals.JwtKeyNotFounded);
+            }
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                };
+            });
+        }
+
+        internal static void CommonCorsConfigurations(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("MyPoliticalCors", policy =>
+                    {
+                        policy.WithOrigins(Globals.DomainPhaser, Globals.DomainStable)
+                              .AllowAnyHeader()
+                              .AllowAnyMethod();
+                    }
+                );
+            });
+        }
+
+        internal static void CommonSwaggerConfigurations(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = Globals.NethAuthCoreValue,
+                    Version = "v1", 
+                    Description = Globals.NethAuthCoreDescription
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' followed by your JWT token. Example: 'Bearer abc123def456'."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+        }
+
+        internal static void CommonConfigure(
+            this IApplicationBuilder app,
+            IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint(Globals.SwaggerUrlEndpointV1, Globals.SwaggerNameEndpointV1);
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error"); // Handles errors in production
+                app.UseHsts(); // Adds additional security for production
+            }
+
+            app.UseCors("MyPoliticalCors");
+
+            //app.UseMiddleware<AuthNetHandleMiddleware>(); // Custom middleware for exception handling
+
+            app.UseHttpsRedirection(); // Redirects HTTP requests to HTTPS
+            app.UseRouting(); // Configures endpoint routing
+            app.UseAuthentication(); // Adds authentication to the pipeline
+            app.UseAuthorization(); // Adds authorization to the pipeline
         }
     }
 }
