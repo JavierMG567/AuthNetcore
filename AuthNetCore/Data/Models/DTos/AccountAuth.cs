b6510be0585ca7df87d374a8f083c668e8f23c74
Javier18121998 <javier.marin.garcia.linkedin01@hotmail.com>
2025-04-03 11:23:28 -0600

    Stablish the correct law of admin the auth of Passwords

---
+++ AuthNetCore/Data/Models/DTos/AccountAuth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthNetCore.Data.Models.DTos
+{
+    public class AccountAuth
+    {
+        public int AccountId { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
